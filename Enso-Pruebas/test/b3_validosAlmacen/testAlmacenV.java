802496c0d98cfc7b163259cfc476834a8d4981ab
raquelvilas18 <raquelvilas18@hotmail.com>
2019-05-04 22:48:23 +0200

    pruebitas

--- Enso-Pruebas/test/b3_validosAlmacen/testAlmacenV.java
+++ Enso-Pruebas/test/b3_validosAlmacen/testAlmacenV.java
@@ -3,6 +3,7 @@ package b3_validosAlmacen;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import elementos.Alarma;
+import elementos.Estadistico;
 import elementos.Medida;
 import elementos.Paciente;
 import subsistemaAlmacenDatos.DatosMedidas;
@@ -17,6 +19,7 @@ import subsistemaAlmacenDatos.Itf1_DatosSimulados;
 import subsistemaAnalisis.DatosAnalisis;
 import subsistemaAnalisis.Itf2_DatosInstantaneos;
 import subsistemaGestionPacientes.DatosPacientes;
+import subsistemaSensorizacion.DatosSensores;
 
 class testAlmacenV {
 
@@ -42,15 +45,119 @@ class testAlmacenV {
 	
 	@Test
 	@DisplayName("CP_00011: Generar alarma con datos de hipotermia")
-	void testenviarDatos_010() {
+	void testgenerarAlarma_011() {
 		Float temp = (float) 34;
 		Float frec = (float)135.0;
 		Medida medida = new Medida(temp, frec, "12-03-2019");
 		Alarma alarmaEsperada = new Alarma("12-03-2019", "T", temp);
 		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
-		assertSame(respuesta, alarmaEsperada);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
 	}
 	
+	@Test
+	@DisplayName("CP_00012: Generar alarma con datos de fiebre")
+	void testgenerarAlarma_012() {
+		Float temp = (float) 39.2;
+		Float frec = (float)135.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "T", temp);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00014: Generar alarma por frecuencia muy baja")
+	void testgenerarAlarma_014() {
+		Float temp = (float) 36.5;
+		Float frec = (float)49.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "F", frec);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
 
+	@Test
+	@DisplayName("CP_00015: Generar alarma por frecuencia muy alta")
+	void testgenerarAlarma_015() {
+		Float temp = (float) 36.5;
+		Float frec = (float)235.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "F", frec);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00051: Generar alarma por temperatura justo inferior al limite")
+	void testgenerarAlarma_051() {
+		Float temp = (float) 35.99;
+		Float frec = (float)235.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "T", temp);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00052: Generar alarma por temperatura justo superior al limite")
+	void testgenerarAlarma_052() {
+		Float temp = (float) 38.01;
+		Float frec = (float)235.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "T", temp);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00054: Generar alarma por frecuencia justo inferior al limite")
+	void testgenerarAlarma_054() {
+		Float temp = (float) 36.5;
+		Float frec = (float)49.99;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "F", frec);
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00056: Generar alarma por frecuencia justo superior al limite")
+	void testgenerarAlarma_056() {
+		Float temp = (float) 36.5;
+		Float frec = (float)132.5;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma alarmaEsperada = new Alarma("12-03-2019", "F", frec);
+		Paciente pacientePrueba = new Paciente("1234567899", "OurenseGalicia", "Maria Mar Alvarez", "01-01-1964");
 
+		Alarma respuesta = subsistema.generarAlarma(medida, pacientePrueba);
+		assertSame(respuesta.getFecha(), alarmaEsperada.getFecha());
+		assertSame(respuesta.getTipo(), alarmaEsperada.getTipo());
+		assertSame(respuesta.getValor(), alarmaEsperada.getValor());
+	}
+	
+	@Test
+	@DisplayName("CP_00018: Generar estadistico valido, con paciente registrado")
+	void testGenerarEstadistico_017() {
+		DatosSensores generador = new DatosSensores();
+		generador.simularDatosSensores(2, pacienteRegistrado.getnSeguridadSocial());
+		Estadistico respuesta = subsistema.generarEstadistico( pacienteRegistrado);
+		for(String dato :respuesta.getDatos().keySet()) {
+			System.out.println(dato +"----" + respuesta.getDatos().get(dato));
+		}
+		//assertNull(respuesta);
+	}
 }
--- Enso-Pruebas/test/b4/noValidosAlmacen/testAlmacenNV.java
+++ Enso-Pruebas/test/b4/noValidosAlmacen/testAlmacenNV.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import elementos.Alarma;
+import elementos.Estadistico;
 import elementos.Medida;
 import elementos.Paciente;
 import subsistemaAnalisis.DatosAnalisis;
@@ -47,11 +48,78 @@ class testAlmacenNV {
 	
 	@Test
 	@DisplayName("CP_00010: Generar alarma con datos normales que no deberian generarla")
-	void testenviarDatos_010() {
+	void testGenerarAlarma_010() {
 		Float temp = (float) 36.2;
 		Float frec = (float)135.0;
 		Medida medida = new Medida(temp, frec, "12-03-2019");
 		Alarma respuesta = subsistema.generarAlarma(null, pacienteRegistrado);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00013: Generar alarma con datos normales, no deberia saltar")
+	void testgenerarAlarma_013() {
+		Float temp = (float) 36.5;
+		Float frec = (float)145.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, pacienteRegistrado);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00016: Generar alarma con datos normales, pero paciente nulo")
+	void testgenerarAlarma_016() {
+		Float temp = (float) 36.5;
+		Float frec = (float)145.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, null);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00049: Generar alarma con datos normales, temperatura en el limite superior")
+	void testgenerarAlarma_049() {
+		Float temp = (float) 38.0;
+		Float frec = (float)145.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, null);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00050: Generar alarma con datos normales, temperatura en el limite inferior")
+	void testgenerarAlarma_050() {
+		Float temp = (float) 36.0;
+		Float frec = (float)145.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, null);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00053: Generar alarma con datos normales,frecuencia el limite inferior")
+	void testgenerarAlarma_053() {
+		Float temp = (float) 36.5;
+		Float frec = (float)50.0;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, null);
+		assertNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00055: Generar alarma con datos normales,frecuencia el limite superior")
+	void testgenerarAlarma_055() {
+		Float temp = (float) 36.5;
+		Float frec = (float)132.5;
+		Medida medida = new Medida(temp, frec, "12-03-2019");
+		Alarma respuesta = subsistema.generarAlarma(medida, null);
+		assertNull(respuesta);
+	}
+
+	@Test
+	@DisplayName("CP_00017: Generar estadistico con  paciente nulo")
+	void testGenerarEstadistico_017() {
+		Estadistico respuesta = subsistema.generarEstadistico( null);
 		assertNull(respuesta);
 	}
 
