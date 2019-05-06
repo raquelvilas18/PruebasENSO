d9e5acbdeba6becc7a011153d13b97012b42fe41
JuanMartinezVillar <>
2019-05-06 12:27:35 +0200

    Test analisis

---
+++ Enso-Pruebas/test/b5_validosAnalisis/testAnalisisV.java
@@ -0,0 +1,14 @@
+package b5_validosAnalisis;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testAnalisisV {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Enso-Pruebas/test/b6_noValidosAnalisis/testAnalisisNV.java
@@ -0,0 +1,14 @@
+package b6_noValidosAnalisis;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testAnalisisNV {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
