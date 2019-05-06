0c8132e63ee32023ff9b86f518974e5a106f2b3f
Luis <luisfl1998@gmail.com>
2019-05-06 13:27:44 +0200

    Casos de prueba válidos de análisis

--- Enso-Pruebas/test/b5_validosAnalisis/testAnalisisV.java
+++ Enso-Pruebas/test/b5_validosAnalisis/testAnalisisV.java
@@ -2,13 +2,66 @@ package b5_validosAnalisis;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import elementos.Alarma;
+import elementos.Estadistico;
+import elementos.Paciente;
+import subsistemaAnalisis.DatosAnalisis;
+import subsistemaAnalisis.Itf4_Estadisticas;
+import subsistemaAnalisis.Itf5_Alarmas;
+import subsistemaGestionPacientes.DatosPacientes;
 
 class testAnalisisV {
 
+	Itf4_Estadisticas subsistema;
+	Itf5_Alarmas subsistema2;
+	String NSSvalidoNoRegistrado = "281234567847";
+	String NSSvalidoRegistrado = "281234567840";
+	Paciente pacienteRegistrado;
+	Paciente pacienteNoRegistrado;
+
+	@BeforeEach
+	void inicio() {
+		subsistema = new DatosAnalisis(new DatosPacientes());
+		subsistema2 = new DatosAnalisis(new DatosPacientes());
+
+		pacienteNoRegistrado = new Paciente(NSSvalidoNoRegistrado, "Santiago de chile5", "Juan Rodriguez Alvarez",
+				"28-07-1998");
+		pacienteRegistrado = new Paciente(NSSvalidoRegistrado, "Santiago de chile5", "Juan Rodriguez Alvarez",
+				"28-07-1998");
+
+		DatosPacientes subsistemaPacientes = new DatosPacientes();
+		subsistemaPacientes.darAlta(pacienteRegistrado);
+	}
+
+	@AfterEach
+	void limpiar() {
+		DatosPacientes subsistemaPacientes = new DatosPacientes();
+		subsistemaPacientes.eliminar(pacienteRegistrado);
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("CP_00031: Solicitar estadístico")
+	void testSolicitarEstadistico_031() {
+		ArrayList<Estadistico> respuesta = null;
+		String fechaInicio = "30-2-2019";
+		String fechaFin = "30-3-2019";
+		respuesta = subsistema.solicitarEstadisticos(pacienteRegistrado, fechaInicio, fechaFin);
+		assertNotNull(respuesta);
+	}
+	
+	@Test
+	@DisplayName("CP_00035: Ver alarmas de un paciente")
+	void testVerAlarmasDeUnPaciente_035() {
+		ArrayList<Alarma> respuesta = null;
+		respuesta = subsistema2.verAlarmas(pacienteRegistrado);
+		assertNotNull(respuesta);
 	}
 
 }
