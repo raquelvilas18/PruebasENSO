1452586e76d71766ee6ffd286e7dc0610c326089
JuanMartinezVillar <>
2019-05-06 13:03:27 +0200

    Update testAnalisisNV.java

--- Enso-Pruebas/test/b6_noValidosAnalisis/testAnalisisNV.java
+++ Enso-Pruebas/test/b6_noValidosAnalisis/testAnalisisNV.java
@@ -15,32 +15,34 @@ import subsistemaAnalisis.Itf5_Alarmas;
 import subsistemaGestionPacientes.DatosPacientes;
 
 class testAnalisisNV {
-	
+
 	Itf4_Estadisticas subsistema;
 	Itf5_Alarmas subsistema2;
 	String NSSvalidoNoRegistrado = "281234567847";
 	String NSSvalidoRegistrado = "281234567840";
 	Paciente pacienteRegistrado;
 	Paciente pacienteNoRegistrado;
-	
+
 	@BeforeEach
 	void inicio() {
 		subsistema = new DatosAnalisis(new DatosPacientes());
 		subsistema2 = new DatosAnalisis(new DatosPacientes());
-		
-		pacienteNoRegistrado = new Paciente(NSSvalidoNoRegistrado,"Santiago de chile5", "Juan Rodriguez Alvarez", "28-07-1998");
-		pacienteRegistrado = new Paciente(NSSvalidoRegistrado, "Santiago de chile5", "Juan Rodriguez Alvarez", "28-07-1998");
-		
-		DatosPacientes subsistemaPacientes= new DatosPacientes();
+
+		pacienteNoRegistrado = new Paciente(NSSvalidoNoRegistrado, "Santiago de chile5", "Juan Rodriguez Alvarez",
+				"28-07-1998");
+		pacienteRegistrado = new Paciente(NSSvalidoRegistrado, "Santiago de chile5", "Juan Rodriguez Alvarez",
+				"28-07-1998");
+
+		DatosPacientes subsistemaPacientes = new DatosPacientes();
 		subsistemaPacientes.darAlta(pacienteRegistrado);
 	}
-	
+
 	@AfterEach
 	void limpiar() {
 		DatosPacientes subsistemaPacientes = new DatosPacientes();
 		subsistemaPacientes.eliminar(pacienteRegistrado);
 	}
-	
+
 	@Test
 	@DisplayName("CP_00032: Solicitar estadístico")
 	void solicitarEstadistico_32() {
@@ -49,8 +51,8 @@ class testAnalisisNV {
 		String fechaFin = "30-3-2019";
 		respuesta = subsistema.solicitarEstadisticos(pacienteNoRegistrado, fechaInicio, fechaFin);
 		assertNull(respuesta);
-	}	
-	
+	}
+
 	@Test
 	@DisplayName("CP_00033: Solicitar estadístico")
 	void solicitarEstadistico_33() {
@@ -60,7 +62,7 @@ class testAnalisisNV {
 		respuesta = subsistema.solicitarEstadisticos(pacienteRegistrado, fechaInicio, fechaFin);
 		assertNull(respuesta);
 	}
-	
+
 	@Test
 	@DisplayName("CP_00034: Solicitar estadístico")
 	void solicitarEstadistico_34() {
@@ -70,13 +72,11 @@ class testAnalisisNV {
 		respuesta = subsistema.solicitarEstadisticos(pacienteRegistrado, fechaInicio, fechaFin);
 		assertNull(respuesta);
 	}
-	
+
 	@Test
 	@DisplayName("CP_00036: Ver alarmas de un paciente")
 	void verAlarmas_36() {
 		ArrayList<Alarma> respuesta = null;
-		String fechaInicio = "30-2-2019";
-		String fechaFin = "30-3-2019";
 		respuesta = subsistema2.verAlarmas(pacienteNoRegistrado);
 		assertNull(respuesta);
 	}
